de98d3381270c296f257c98aee4fd4dfbc61e5eb
Jarrett Goldfedder <JGoldfed@msn.com>
2016-07-09 15:26:59 -0400

    Adding files for multi-vm setup

---
+++ scripts/centos-web.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+
+chkconfig  httpd on
+service httpd stop
+
+rm -rf /var/www/html
+
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+#PHP
+
+yum install -y php php-cli php-common php-devel php-mysql
+service httpd restart
+
+
